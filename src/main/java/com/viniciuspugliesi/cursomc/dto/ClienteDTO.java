eee8bce11474c5eddef4b6e2bddd4eca97473ab6
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-08-31 12:06:02 -0300

    PUT and DELETE for clientes

---
+++ src/main/java/com/viniciuspugliesi/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,58 @@
+package com.viniciuspugliesi.cursomc.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.viniciuspugliesi.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório.")
+	@Length(min=5, max=120, message="O tamanho deve ser entr 5 e 120 caracteres.")
+	private String nome;
+
+	@NotEmpty(message="Preenchimento obrigatório.")
+	@Email(message="Email inválido.")
+	private String email;
+	
+	public ClienteDTO() {
+	}
+	
+	public ClienteDTO(Cliente cliente) {
+		this.id = cliente.getId();
+		this.nome = cliente.getNome();
+		this.email = cliente.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
--- src/main/java/com/viniciuspugliesi/cursomc/services/ClienteService.java
+++ src/main/java/com/viniciuspugliesi/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.viniciuspugliesi.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.viniciuspugliesi.cursomc.domain.Cliente;
+import com.viniciuspugliesi.cursomc.dto.ClienteDTO;
 import com.viniciuspugliesi.cursomc.repositories.ClienteRepository;
+import com.viniciuspugliesi.cursomc.services.exceptions.DataIntegrityException;
 import com.viniciuspugliesi.cursomc.services.exceptions.ObjectNotFountException;
 
 @Service
@@ -12,6 +20,10 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repository;
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
 	
 	public Cliente find(Integer id) {
 		Cliente obj = repository.findOne(id);
@@ -21,5 +33,44 @@ public class ClienteService {
 		}
 		
 		return obj;
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+	
+	public Cliente update(Integer id, Cliente obj) {
+		Cliente newObj = this.find(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		this.find(id);
+		
+		try {
+			repository.delete(id);	
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos.");
+		}
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		if (page != 0) {
+			page -= 1;
+		}
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
