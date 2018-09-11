f6b27d995a59465b1d56bfc5ad8cf617bdffccfd
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-09-11 15:37:33 -0300

    Custom Validation email

--- src/main/java/com/viniciuspugliesi/cursomc/services/validations/ClienteInsertValidator.java
+++ src/main/java/com/viniciuspugliesi/cursomc/services/validations/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.viniciuspugliesi.cursomc.domain.enums.TipoCliente;
 import com.viniciuspugliesi.cursomc.dto.ClienteNewDTO;
+import com.viniciuspugliesi.cursomc.repositories.ClienteRepository;
 import com.viniciuspugliesi.cursomc.resources.exceptions.FieldMessage;
 import com.viniciuspugliesi.cursomc.services.validations.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -20,6 +27,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 
+		if (clienteRepository.findByEmail(objDto.getEmail()) != null) {
+			list.add(new FieldMessage("email", "Email já existente."));
+		}
+		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido."));
 		}
